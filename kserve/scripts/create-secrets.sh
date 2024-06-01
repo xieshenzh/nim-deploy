4d1b59431f8da5a25fc141e8d95ad56c0acca102
Xieshen Zhang <xiezhang@redhat.com>
2024-06-01 00:00:12 -0400

    PoC

--- kserve/scripts/create-secrets.sh
+++ kserve/scripts/create-secrets.sh
@@ -20,14 +20,14 @@ if [ -z "$NGC_API_KEY" ]; then
   exit 1
 fi
 
-kubectl create secret docker-registry ngc-secret \
+oc create secret docker-registry ngc-secret \
  --docker-server=nvcr.io\
  --docker-username='$oauthtoken'\
  --docker-password=${NGC_API_KEY}
 
 # Encode the tokens to base64
-HF_TOKEN_BASE64=$(echo -n "$HF_TOKEN" | base64 -w0)
-NGC_API_KEY_BASE64=$(echo -n "$NGC_API_KEY" | base64 -w0)
+HF_TOKEN_BASE64=$(echo -n "$HF_TOKEN" | base64)
+NGC_API_KEY_BASE64=$(echo -n "$NGC_API_KEY" | base64)
 
 # Replace placeholders in YAML and apply
-sed -e "s|\${HF_TOKEN}|${HF_TOKEN_BASE64}|g" -e "s|\${NGC_API_KEY}|${NGC_API_KEY_BASE64}|g" ${SCRIPT_DIR}/nvidia-nim-secrets.yaml | kubectl apply -f -
+sed -e "s|\${HF_TOKEN}|${HF_TOKEN_BASE64}|g" -e "s|\${NGC_API_KEY}|${NGC_API_KEY_BASE64}|g" ${SCRIPT_DIR}/nvidia-nim-secrets.yaml | oc apply -f -
